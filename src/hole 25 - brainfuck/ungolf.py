d23cf83d4c50d568aeefeb54dcc8428e8074f978
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-03 12:28:14 -0800

    Update ungolf.py

--- src/hole 25 - brainfuck/ungolf.py
+++ src/hole 25 - brainfuck/ungolf.py
@@ -1,8 +1,11 @@
             import sys
             for ins in sys.argv[1:]:
+                ins=[_ for _ in ins if _ in"[<=.,->]"]
                 mem=[0];ip=mp=0
                 while 1:
                     i=ins[ip]
+                    if i==",":
+                        mem[mp]=ord(input()[0])
                     if i==".":
                         print(chr(mem[mp]),end="")
                     elif i in"+-":
