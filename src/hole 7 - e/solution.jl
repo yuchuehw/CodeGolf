0905b6b3e86e03f999561434c42925df02341be2
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-02 18:41:44 -0800

    Update solution.jl

--- src/hole 7 - e/solution.jl
+++ src/hole 7 - e/solution.jl
@@ -1,3 +1,5 @@
+# get helped from this post https://discourse.julialang.org/t/constants-like-e-and-eulergamma-in-julia-v1-0-no-longer-available/15917
+# note you must use ℯ and not e.
 setprecision(Int(ceil(log2(10)*2000))) do
     println(string(BigFloat(ℯ))[1:1002])
 end
