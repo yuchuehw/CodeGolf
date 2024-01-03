20e76856f729eddb03e25316a0557389b412b648
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-03 12:13:50 -0800

    Update ungolf.py

--- src/hole 25 - brainfuck/ungolf.py
+++ src/hole 25 - brainfuck/ungolf.py
@@ -1,34 +1,27 @@
-import sys
-for ins in sys.argv[1:]:
-	mem=[0];ip=mp=0
-	while 1:
-		i=ins[ip]
-		if i==".":
-			print(chr(mem[mp]),end="")
-			ip+=1
-		elif i=="+":
-			mem[mp]+=1
-			ip+=1
-		elif i=="-":
-			mem[mp]-=1
-			ip+=1
-		elif i==">":
-			mp+=1
-			mem+=[0]if len(mem)==mp else[]
-			ip+=1
-		elif i=="<":
-			mp,mem=(mp-1,mem)if mp else(0,[0]+mem)
-			ip+=1
-		elif i in"[]":
-			f=mem[mp]
-			f,_,__,inc=[f==0,1,0,1]if i=="["else[f,0,1,-1]
-			if f:
-				while _!=__:
-					ip+=inc
-					if ins[ip]=="[":
-						_+=1
-					if ins[ip]=="]":
-						__+=1
-			ip+=1
-		if ip==len(ins):
-			break
+            import sys
+            for ins in sys.argv[1:]:
+                mem=[0];ip=mp=0
+                while 1:
+                    i=ins[ip]
+                    if i==".":
+                        print(chr(mem[mp]),end="")
+                    elif i in"+-":
+                        exec(f"mem[mp]{i}=1")
+                    elif i==">":
+                        mp+=1
+                        mem+=[0]if len(mem)==mp else[]
+                    elif i=="<":
+                        mp,mem=(mp-1,mem)if mp else(0,[0]+mem)
+                    elif i in"[]":
+                        f=mem[mp]
+                        f,_,__,inc=[f==0,1,0,1]if i=="["else[f,0,1,-1]
+                        if f:
+                            while _!=__:
+                                ip+=inc
+                                if ins[ip]=="[":
+                                    _+=1
+                                if ins[ip]=="]":
+                                    __+=1
+                    ip+=1
+                    if ip==len(ins):
+                        break
