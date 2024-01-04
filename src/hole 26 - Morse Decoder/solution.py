4f5bd845ad26b4a9224f90ba394bcb2ce9519b0d
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-04 14:38:36 -0800

    Update solution.py

--- src/hole 26 - Morse Decoder/solution.py
+++ src/hole 26 - Morse Decoder/solution.py
@@ -1,9 +1,9 @@
 """
 see the generation of string s in magic-s-generate.py
-as of 01/04/2023 solution ranked 77th in bytes and 93rd in chrs.
+as of 01/04/2023 solution ranked 61st in bytes and 74th in chrs.
 """
 import sys
-s="..ETINAMSDRGUKWOHBLZFCP.VX.Q.YJ.56.7...8.......94.......3...2.10";d={s.index(i):i for i in s};r=str.replace
+s="..ETINAMSDRGUKWOHBLZFCP.VX.Q.YJ.56.7...8.......94.......3...2.10";r=str.replace
 for w in sys.argv[1].split(" "*10):
-	for c in w.split(" "*3):print(d[int("1"+r(r(r(c,"▄"*3,"1"),"▄","0")," ","")[::-1],2)],end="")
+	for c in w.split(" "*3):print(s[int("1"+r(r(r(c,"▄"*3,"1"),"▄","0")," ","")[::-1],2)],end="")
 	print(" ",end="")
