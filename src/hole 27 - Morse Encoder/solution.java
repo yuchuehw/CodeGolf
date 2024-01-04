592946e8589fe8c1c42183bff36303d1ff91165f
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-04 15:20:23 -0800

    Create solution.java

---
+++ src/hole 27 - Morse Encoder/solution.java
@@ -0,0 +1,1 @@
+class M{public static void main(String[]a){String s="..ETINAMSDRGUKWOHBLZFCP.VX.Q.YJ.56.7...8.......94.......3...2.10";for(String w:a[0].split(" ")){for(char c:w.toCharArray()){System.out.print(new StringBuffer(Integer.toBinaryString(s.indexOf(c)).substring(1)).reverse().toString().replace("0","▄ ").replace("1","▄▄▄ ")+"  ");}System.out.print(" ".repeat(7));}}}
