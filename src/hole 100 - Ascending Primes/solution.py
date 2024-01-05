ed81efa6b04ee5f8eaf27ef10a3408d338e9519c
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-04 18:24:18 -0800

    Update solution.py

--- src/hole 100 - Ascending Primes/solution.py
+++ src/hole 100 - Ascending Primes/solution.py
@@ -7,6 +7,6 @@ the while loop create candidate by converting i(2~2**9) to binary and use 0 and 
 as of 01/02/2024 the code rank 35th in bytes and chars. 
 """
 i,s,d=1,"123456789",{}
-while i<2**9:d[int("".join([s[n]for n,i in enumerate(bin(i)[2:][::-1])if i=="1"]))]= 0;i+=1
+while i<512:d[int("".join([s[n]for n,i in enumerate(bin(i)[2:][::-1])if i=="1"]))]=0;i+=1
 for i in sorted(d)[1:]:
 	if all(i%j for j in range(2,i)):print(i)
