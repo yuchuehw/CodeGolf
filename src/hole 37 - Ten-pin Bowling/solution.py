d893ecb94779c283533b3b48c76745221a3e78ee
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-11 14:42:10 -0800

    Create solution.py

---
+++ src/hole 37 - Ten-pin Bowling/solution.py
@@ -0,0 +1,11 @@
+"""
+assume all input is good. as of 01/11/2023 solution ranked 63 in bytes and chrs.
+"""
+import sys
+for a in sys.argv[1:]:
+	s=p=0;m=[1]*99;j=-1
+	for c in a:
+		j+=1
+		if c==" ":continue
+		c=c.replace("⑤","5").replace("⑥","6").replace("⑦","7").replace("⑧","8");m[1]+=1if c in"X/"and j<27else 0;m[2]+=1if c=="X"and j<27else 0;c=10 if c=="X"else 10-p if c=="/"else int(c) if c.isnumeric()else 0;s+=m.pop(0)*c;p=c
+	print(s)
