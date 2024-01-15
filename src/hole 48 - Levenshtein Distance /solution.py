50903b1ad7a9a638fff3c535b1accb2b3dd906f3
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-14 21:25:45 -0800

    Update solution.py

--- src/hole 48 - Levenshtein Distance /solution.py
+++ src/hole 48 - Levenshtein Distance /solution.py
@@ -1,3 +1,7 @@
+"""
+optimizaton of recursive Levenshtein distance algorithm from https://www.geeksforgeeks.org/introduction-to-levenshtein-distance/
+as of 01/14/2024 ranked 110th in bytes.
+"""
 import sys
 def l(s,S):i,j=s[:-1],S[:-1];return len(S)if s==""else len(s)if S==""else l(i,j)if s[-1]==S[-1]else 1+min(l(i,S),l(s,j),l(i,j))
 for a in sys.argv[1:]:print(l(*a.split(" ")))
