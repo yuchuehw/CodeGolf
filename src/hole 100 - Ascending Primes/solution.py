eebc4919833333ebdb46d6ed43737b185be594e0
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-17 15:20:00 -0800

    Update solution.py

--- src/hole 100 - Ascending Primes/solution.py
+++ src/hole 100 - Ascending Primes/solution.py
@@ -6,7 +6,7 @@ since candidate must be in ascending order there's only 2**9(512) options. bigge
 the while loop create candidate by converting i(2~2**9) to binary and use 0 and 1 to represent the absent or present of the 9 numbers.
 as of 01/02/2024 the code rank 35th in bytes and chars. 
 """
-i,s,d=1,"123456789",{}
-while i<512:d[int("".join([s[n]for n,i in enumerate(bin(i)[2:][::-1])if i=="1"]))]=0;i+=1
+i,s,d=1,"123456789",[]
+while i<512:d+=[int("".join(s[n]for n,i in enumerate(bin(i)[:1:-1])if i=="1"))];i+=1
 for i in sorted(d)[1:]:
 	if all(i%j for j in range(2,i)):print(i)
