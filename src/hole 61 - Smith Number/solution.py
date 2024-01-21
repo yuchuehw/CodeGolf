20863c96fea5acc7ba15e1fb23d2534b170d3b62
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-21 13:21:08 -0800

    Update solution.py

--- src/hole 61 - Smith Number/solution.py
+++ src/hole 61 - Smith Number/solution.py
@@ -7,3 +7,6 @@ while i<1e4:
 	for k in range(2,i):
 		while j/k==j//k:j/=k;s+=str(k)
 	if sum(map(int,str(i)))==sum(map(int,s)):print(i)
+
+## byte version ##
+# exec(bytes('㵩ਲ਼桷汩⁥㱩攱㨴ऊ⭩ㄽ樻椽猻∽ਢ昉牯欠椠⁮慲杮⡥ⰲ⥩਺उ桷汩⁥⽪㵫樽⼯㩫⽪欽猻㴫瑳⡲⥫ऊ晩猠浵洨灡椨瑮猬牴椨⤩㴩猽浵洨灡椨瑮猬⤩瀺楲瑮椨 ','u16')[2:])
