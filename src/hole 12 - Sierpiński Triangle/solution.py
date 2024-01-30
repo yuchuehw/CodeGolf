5fba234e06c1467d8536d0968e5f3b46b32db348
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-30 15:08:18 -0800

    Update solution.py

--- src/hole 12 - Sierpiński Triangle/solution.py
+++ src/hole 12 - Sierpiński Triangle/solution.py
@@ -1,7 +1,8 @@
 '''
 explanation:
 the function t return xth-order sierpinski triangle (x>=0) but upside down. the final print would flip the triangle right side up.
-ranking as of 1/2/2024 200th in bytes. 202th in chars.
+ranking as of 1/30/2024 188th in bytes
 '''
-def t(x):p=t(x-1)if x else 0;return[c+" "*(i+1)+c for i,c in enumerate(p)]+[" "*len(p)+c for c in p]if x else['▲']
-print(*t(4)[::-1],sep="\n")
+t=lambda x:[c+" "*(i+1)+c for i,c in enumerate(t(x-1))]+[" "*2**(x-1)+c for c in t(x-1)]if x else['▲'];*map(print,t(4)[::-1]),
+# slightly more functional approach
+# *map(print,(lambda a:lambda v:a(a,v))(lambda t,x:[c+" "*(i+1)+c for i,c in enumerate(t(t,x-1))]+[" "*2**(x-1)+c for c in t(t,x-1)]if x else['▲'])(4)[::-1]),
